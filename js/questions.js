17c6f186be2761e028904de79a7deecce2b3f6e6
hamdijerbi <80006794+hamdijerbi@users.noreply.github.com>
2023-01-15 13:17:37 +0000

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,144 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Que signifie HTML ?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Que signifie CSS ?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Que signifie PHP ?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Que signifie SQL ?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Qu'est ce qu'un navigateur web ?",
+    answer: "Un logiciel capable d'interpréter et d'afficher du code HTML",
+    options: [
+      "Un logiciel capable d'interpréter et d'afficher du code HTML",
+      "Un réseau social global",
+      "La toute première application mondiale",
+      "Une interface pour lire et recevoir des e-mails"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Comment représenter une liste en HTML ?",
+    answer: "ul",
+    options: [
+      "h1",
+      "div",
+      "ul",
+      "list"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Quel attribut permet d'indiquer la destination d'un lien ?",
+    answer: "href",
+    options: [
+      "link",
+      "href",
+      "src",
+      "a"
+    ]
+  }, {
+    numb: 8,
+    question: "À quoi sert le langage CSS ?",
+    answer: "À ajouter du style aux documents web",
+    options: [
+      "À réaliser des pages dynamiques",
+      "À ajouter du style aux documents web",
+      "À insérer du contenu dans une page internet",
+      "À simuler une application en mode avion durant les trajets dans un bus"
+    ]
+  }, {
+    numb: 9,
+    question: "À quoi sert le sélecteur de classe : h2.nomDeClasse ?",
+    answer: "À définir différentes propriétés pour l'élément dont la classe est 'nomDeClasse'.",
+    options: [
+      "Ah bon ... C'est censé servir à quelque chose ?",
+      "À créer une ancre sur la balise <h2>",
+      "À définir différentes propriétés pour l'élément dont la classe est 'nomDeClasse'.",
+      "À ajouter un peu de classe à notre code parfois un peu tristounet"
+    ]
+  }, {
+    numb: 10,
+    question: "Laquelle de ces syntaxes est correcte ?",
+    answer: "if (a != 2) {}",
+    options: [
+      "if (a != 2) {}",
+      "if a != 2 {}",
+      "if (a <> 2) {}",
+      "if a <> 2 {}"
+    ]
+  }, {
+    numb: 11,
+    question: "Laquelle de ces propositions est un nom de variable valide en JavaScript ?",
+    answer: "$b",
+    options: [
+      "var",
+      "$b",
+      "NaN",
+      "default"
+    ]
+  }, {
+    numb: 12,
+    question: "var iNum = 12; iNum %= 2; A la suite de cette expression, combien vaut iNum ?",
+    answer: "0",
+    options: [
+      "6",
+      "14",
+      "0.12",
+      "0"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
